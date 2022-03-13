109d7d9ee78d5e22eaf94aa3fbc0c7f2a3734f21
Kyle <k7for9.eckrich@gmail.com>
2022-03-13 18:37:35 -0500

    lets go

--- kyleswinningbot.py
+++ kyleswinningbot.py
@@ -13,13 +13,13 @@ import random
 class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
     starting_nexus: Unit
     async def on_step(self, iteration: int): # on_step is a method that is called every step of the game.
-        print(
-            f"{iteration}, n_workers: {self.workers.amount}, n_idle_workers: {self.workers.idle.amount},", \
-            # f"minerals: {self.minerals}, gas: {self.vespene}, cannons: {self.structures(UnitTypeId.PHOTONCANNON).amount},", \
-            f"pylons: {self.structures(UnitTypeId.PYLON).amount}, nexus: {self.structures(UnitTypeId.NEXUS).amount}", \
-            f"gateways: {self.structures(UnitTypeId.GATEWAY).amount}, cybernetics cores: {self.structures(UnitTypeId.CYBERNETICSCORE).idle.amount}", \
-            # f"stargates: {self.structures(UnitTypeId.STARGATE).amount}, voidrays: {self.units(UnitTypeId.VOIDRAY).amount}, supply: {self.supply_used}/{self.supply_cap}", \
-            "done")
+        # print(
+        #     f"{iteration}, n_workers: {self.workers.amount}, n_idle_workers: {self.workers.idle.amount},", \
+        #     # f"minerals: {self.minerals}, gas: {self.vespene}, cannons: {self.structures(UnitTypeId.PHOTONCANNON).amount},", \
+        #     f"pylons: {self.structures(UnitTypeId.PYLON).amount}, nexus: {self.structures(UnitTypeId.NEXUS).amount}", \
+        #     f"gateways: {self.structures(UnitTypeId.GATEWAY).amount}, cybernetics cores: {self.structures(UnitTypeId.CYBERNETICSCORE).idle.amount}", \
+        #     # f"stargates: {self.structures(UnitTypeId.STARGATE).amount}, voidrays: {self.units(UnitTypeId.VOIDRAY).amount}, supply: {self.supply_used}/{self.supply_cap}", \
+        #     "done")
 
         last_expanstion: int = 0
         global starting_nexus
@@ -136,10 +136,10 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                     self.research(UpgradeId.PROTOSSAIRWEAPONSLEVEL1)
                 elif self.can_afford(UpgradeId.PROTOSSAIRARMORSLEVEL1):
                     self.research(UpgradeId.PROTOSSAIRARMORSLEVEL1)
-                # elif self.can_afford(UpgradeId.PROTOSSAIRWEAPONSLEVEL2):
-                #     self.research(UpgradeId.PROTOSSAIRWEAPONSLEVEL2)
-                # elif self.can_afford(UpgradeId.PROTOSSAIRARMORSLEVEL2):
-                #     self.research(UpgradeId.PROTOSSAIRARMORSLEVEL2)
+                elif self.can_afford(UpgradeId.PROTOSSAIRWEAPONSLEVEL2):
+                    self.research(UpgradeId.PROTOSSAIRWEAPONSLEVEL2)
+                elif self.can_afford(UpgradeId.PROTOSSAIRARMORSLEVEL2):
+                    self.research(UpgradeId.PROTOSSAIRARMORSLEVEL2)
 
                 # build a forge if there is not one
                 if self.already_pending(UnitTypeId.FORGE) + self.structures.filter(lambda structure: structure.type_id == UnitTypeId.FORGE).amount == 0:
@@ -174,13 +174,17 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
 
             if self.supply_used > 190:
                 if self.enemy_units:
-                    for vr in self.units(UnitTypeId.VOIDRAY):
+                    for vr in self.units(UnitTypeId.VOIDRAY).idle:
                         vr.attack(random.choice(self.enemy_units))
                 elif self.enemy_structures:
                     for vr in self.units(UnitTypeId.VOIDRAY):
                         vr.attack(random.choice(self.enemy_structures))
+            elif self.units(UnitTypeId.VOIDRAY).amount > 6:
+                for vr in self.units(UnitTypeId.VOIDRAY):
+                    pos: Point2 = (self.main_base_ramp.protoss_wall_pylon + self.game_info.map_center) / 2
+                    vr.attack(pos)
             else:
-                 for vr in self.units(UnitTypeId.VOIDRAY):
+                 for vr in self.units(UnitTypeId.VOIDRAY).idle:
                     pos: Point2 = (self.main_base_ramp.protoss_wall_pylon + self.game_info.map_center) / 2
                     vr.attack(pos)
 
@@ -188,97 +192,20 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
         # if iteration == 500: # we are in the opener 
         #     x = 1
 
-        '''
-        if self.townhalls:  # do we have a nexus?
-            nexus = self.townhalls.random  # select one (will just be one for now)
-
-
-            # if we have less than 10 voidrays, build one:
-            if self.structures(UnitTypeId.VOIDRAY).amount < 10 and self.can_afford(UnitTypeId.VOIDRAY):
-                for sg in self.structures(UnitTypeId.STARGATE).ready.idle:
-                    if self.can_afford(UnitTypeId.VOIDRAY):
-                        sg.train(UnitTypeId.VOIDRAY)
-
-
-            # leave room to build void rays
-            supply_remaining = self.supply_cap - self.supply_used
-            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE) and supply_remaining > 4:
-                nexus.train(UnitTypeId.PROBE)
-
-
-            # if we dont have *any* pylons, we'll build one close to the nexus.
-            elif not self.structures(UnitTypeId.PYLON) and self.already_pending(UnitTypeId.PYLON) == 0:
-                if self.can_afford(UnitTypeId.PYLON):
-                    await self.build(UnitTypeId.PYLON, near=nexus)
-
-
-            elif self.structures(UnitTypeId.PYLON).amount < 5:
-                if self.can_afford(UnitTypeId.PYLON):
-                    # build from the closest pylon towards the enemy
-                    target_pylon = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0])
-                    # build as far away from target_pylon as possible:
-                    pos = target_pylon.position.towards(self.enemy_start_locations[0], random.randrange(8, 15))
-                    await self.build(UnitTypeId.PYLON, near=pos)
-
-            
-            
-            elif self.structures(UnitTypeId.ASSIMILATOR).amount <= 1:
-                for nexus in self.structures(UnitTypeId.NEXUS):
-                    vespenes = self.vespene_geyser.closer_than(15, nexus)
-                    for vespene in vespenes:
-                        if self.can_afford(UnitTypeId.ASSIMILATOR) and not self.already_pending(UnitTypeId.ASSIMILATOR):
-                            await self.build(UnitTypeId.ASSIMILATOR, vespene)
-
-
-
-
-            elif not self.structures(UnitTypeId.FORGE):  # if we don't have a forge:
-                if self.can_afford(UnitTypeId.FORGE):  # and we can afford one:
-                    # build one near the Pylon that is closest to the nexus:
-                    await self.build(UnitTypeId.FORGE, near=self.structures(UnitTypeId.PYLON).closest_to(nexus))
-
-            # if we have less than 3 cannons, let's build some more if possible:
-            elif self.structures(UnitTypeId.FORGE).ready and self.structures(UnitTypeId.PHOTONCANNON).amount < 3:
-                if self.can_afford(UnitTypeId.PHOTONCANNON):  # can we afford a cannon?
-                    await self.build(UnitTypeId.PHOTONCANNON, near=nexus)  # build one near the nexus
-
-
-            # a gateway? this gets us towards cyb core > stargate > void ray
-            elif not self.structures(UnitTypeId.GATEWAY):
-                if self.can_afford(UnitTypeId.GATEWAY):
-                    await self.build(UnitTypeId.GATEWAY, near=self.structures(UnitTypeId.PYLON).closest_to(nexus))
-            
-            # a cyber core? this gets us towards stargate > void ray
-            elif not self.structures(UnitTypeId.CYBERNETICSCORE):
-                if self.can_afford(UnitTypeId.CYBERNETICSCORE):
-                    await self.build(UnitTypeId.CYBERNETICSCORE, near=self.structures(UnitTypeId.PYLON).closest_to(nexus))
-
-            # a stargate? this gets us towards void ray
-            elif not self.structures(UnitTypeId.STARGATE):
-                if self.can_afford(UnitTypeId.STARGATE):
-                    await self.build(UnitTypeId.STARGATE, near=self.structures(UnitTypeId.PYLON).closest_to(nexus))
-            
-
-
-        else:
-            if self.can_afford(UnitTypeId.NEXUS):  # can we afford one?
-                await self.expand_now()  # build one!
-        '''      
-
         # if we have more than 3 voidrays, let's attack!
-        if self.units(UnitTypeId.VOIDRAY).amount >= 3:
-            if self.enemy_units:
-                for vr in self.units(UnitTypeId.VOIDRAY).idle:
-                    vr.attack(random.choice(self.enemy_units))
+        # if self.units(UnitTypeId.VOIDRAY).amount >= 3:
+        #     if self.enemy_units:
+        #         for vr in self.units(UnitTypeId.VOIDRAY).idle:
+        #             vr.attack(random.choice(self.enemy_units))
             
-            elif self.enemy_structures:
-                for vr in self.units(UnitTypeId.VOIDRAY).idle:
-                    vr.attack(random.choice(self.enemy_structures))
+        #     elif self.enemy_structures:
+        #         for vr in self.units(UnitTypeId.VOIDRAY).idle:
+        #             vr.attack(random.choice(self.enemy_structures))
 
-            # otherwise attack enemy starting position
-            else:
-                for vr in self.units(UnitTypeId.VOIDRAY).idle:
-                    vr.attack(self.enemy_start_locations[0])
+        #     # otherwise attack enemy starting position
+        #     else:
+        #         for vr in self.units(UnitTypeId.VOIDRAY).idle:
+        #             vr.attack(self.enemy_start_locations[0])
 
     async def on_start(self):
         print("starting")
@@ -289,7 +216,7 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
 run_game(  # run_game is a function that runs the game.
     maps.get("2000AtmospheresAIE"), # the map we are playing on
     [Bot(Race.Protoss, IncrediBot()), # runs our coded bot, protoss race, and we pass our bot object 
-     Computer(Race.Random, Difficulty.Harder)], # runs a pre-made computer agent, zerg race, with a hard difficulty.
+     Computer(Race.Terran, Difficulty.Harder)], # runs a pre-made computer agent, zerg race, with a hard difficulty.
     realtime=False, # When set to True, the agent is limited in how long each step can take to process.
     disable_fog=True,
 )
